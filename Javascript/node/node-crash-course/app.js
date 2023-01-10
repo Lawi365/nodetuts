9ca5a1ad635c932fab644a5137cc8f43464d3651
lawi365 <lawi365@pm.me>
2023-01-09 16:54:52 -0800

    Main Added mongoDB , now using mongoose to create and update

--- Javascript/node/node-crash-course/app.js
+++ Javascript/node/node-crash-course/app.js
@@ -1,22 +1,26 @@
 const express = require('express');
-const mongoose = require('mongoose')
-//3rd party middleware.
+const mongoose = require('mongoose');
+const Blog = require('./models/blog');
 
+//3rd party middleware.
 const morgan = require('morgan');
 
 // express app
 const app = express();
 
 //connect to mongondb
-const dburl = 'mongodb+srv://admin:<Nodetuts123>@nodetuts.ksmyxi0.mongodb.net/?retryWrites=true&w=majority'
-mongoose.connect(dburl)
+const dburl = 'mongodb+srv://admin:Nodetuts123@nodetuts.ksmyxi0.mongodb.net/?retryWrites=true&w=majority'
+mongoose.connect(dburl,{useNewUrlParser: true, useUnifiedTopology: true})
+    .then((result) => app.listen(3000))
+    .catch((err)=> console.log(err));
+
 //registering view engines
 
 app.set('view engine','ejs');
 // app.set('views','myviews');
 
 //listen for requests
-app.listen(3000);
+
 
 //add middleware.
 // app.use((req,res,next)=>{
@@ -34,12 +38,47 @@ app.listen(3000);
 app.use(express.static('public'))
 app.use(morgan('dev'));
 
+//mongoose and mongo sandbox routes.
+app.get('/add-blog',(req, res)=>{
+    const blog = new Blog({
+        title: 'new blog 2',
+        snippet: 'about my new blog',
+        body: 'more about the new blog follow along'
+    });
+    blog.save()
+    .then((result)=>{
+        res.send(result)
+    })
+    .catch((err)=>{
+        console.log(err)
+    })
+})
+
+app.get('/all-blogs',(req,res)=>{
+    Blog.find()
+        .then((result)=>{
+            res.send(result);
+        })
+        .catch((err)=>{
+            console.log(err)
+        })
+})
+
+app.get('/single-blog',(req,res)=>{
+    Blog.findById('63bc9aa574a399c2afeb20e4')
+        .then((result)=>{
+            res.send(result)
+        })
+        .catch((err)=>{
+            console.log(err)
+        })
+})
+
 app.get('/',(req,res)=>{
     const blogs = [
         {title:'Lawrence Begins to blog',snippet:'It may come as a suprise but did you know that I write blogs'},
         {title:'Lawrence Continues to blog',snippet:'It may come as a suprise but did you know that I write blogs'},
-        {title:'Lawrence Finished blogging',snippet:'It may come as a suprise but did you know that I write blogs'},
-       
+        {title:'Lawrence Finished blogging',snippet:'It may come as a suprise but did you know that I write blogs'}
     ];
 
     // res.send('<p>home page</p>');
